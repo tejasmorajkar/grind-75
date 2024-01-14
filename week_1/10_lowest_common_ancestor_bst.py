cab6a8d271e596a5d941d983bd0041d5ec6fde47
tejasmorajkar <tmorajkar316@gmail.com>
2024-01-15 00:53:02 +0530

    solve lowest common ancestor for binary search tree

---
+++ week_1/10_lowest_common_ancestor_bst.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowest_common_ancestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        curr = root
+        while curr:
+            if curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            elif curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            else:
+                return curr
+        
+
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.right = TreeNode(8)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(4)
+root.left.right.left = TreeNode(3)
+root.left.right.right = TreeNode(5)
+root.right.left = TreeNode(7)
+root.right.right = TreeNode(9)
+
+result = Solution().lowest_common_ancestor(root, TreeNode(2), TreeNode(8))
+print(result.val)
+
+result = Solution().lowest_common_ancestor(root, TreeNode(2), TreeNode(4))
+print(result.val)
